c582affb623ed237b2dfafa0aac19f19c8fc491b
Otabek Azamov <70770451+xiiforlearning@users.noreply.github.com>
2020-10-19 21:33:36 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+  for (const el of str.split``) {
+      if (stack[0] === el) {
+          stack.shift();
+      } else if (brackets[el]) {
+          stack.unshift(brackets[el]);
+      } else {
+          return false;
+      }
+  }
+  return stack.length === 0;
 }
